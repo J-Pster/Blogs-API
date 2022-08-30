df1ee508615c1cf7d34ca206bfa9dc20529c0660
Joaopster <jpster@pop-os.localdomain>
2022-08-30 11:32:33 -0300

    Criada a Primeira Model User.js

---
+++ src/database/models/user.js
@@ -0,0 +1,35 @@
+/**
+* @param {import('sequelize').DataTypes} DataTypes
+* @param {import('sequelize').Sequelize } Sequelize
+* @returns
+*/
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validator: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+
+  return User;
+};
+
+module.exports = User;
