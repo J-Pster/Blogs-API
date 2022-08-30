a4aecdcc68ab9ebb278461a6d61740d1d83d9db6
Joaopster <jpster@pop-os.localdomain>
2022-08-30 11:37:39 -0300

    Criada a Model Category para a Table Categories

---
+++ src/database/models/category.js
@@ -0,0 +1,23 @@
+/**
+* @param {import('sequelize').DataTypes} DataTypes
+* @param {import('sequelize').Sequelize } Sequelize
+* @returns
+*/
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+
+  return Category;
+};
+
+module.exports = Category;
